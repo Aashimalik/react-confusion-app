02c8cda5002e90aff3af9e7c7b8f9dc98a337db4
ayesha_malik <ayesha_malik@seologistics.com>
2018-08-24 11:50:08 +0530

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,10 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
+
+
+
 class Main extends Component {
 
     constructor(props) {
@@ -18,13 +22,10 @@ class Main extends Component {
             dishes: DISHES,
             comments: COMMENTS,
             promotions: PROMOTIONS,
-            leaders: LEADERS
+            leaders: LEADERS,
         }
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId});
-      }
 
     render() {
         const HomePage=()=>{
@@ -36,6 +37,12 @@ class Main extends Component {
           />
             )
         }
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+        }
         return (
             <div>
             <Header />
@@ -43,6 +50,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route exact path='/contactus' component={Contact} />} />
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Redirect to="/home" />
             </Switch>
             <Footer />
