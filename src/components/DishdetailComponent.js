ac1dd16c5b616296b593c68fc81a3bdeefb2d52c
ayesha_malik <ayesha_malik@seologistics.com>
2018-09-06 15:14:46 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -35,8 +35,7 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log("Current State is: " + JSON.stringify(values));
-    alert("Current State is: " + JSON.stringify(values));
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   toggleModal() {
@@ -140,7 +139,7 @@ function RenderDish({ dish }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments,addComment, dishId }) {
   return (
     <div>
       <h4>Comments</h4>
@@ -159,12 +158,13 @@ function RenderComments({ comments }) {
           );
         })}
       </ul>
-      <CommentForm />
+      <CommentForm dishId={dishId} addComment={addComment} />
     </div>
   );
 }
 
 const DishDetail = props => {
+    
   if (props.dish != null) {
     return (
       <div className="container">
@@ -185,7 +185,10 @@ const DishDetail = props => {
             <RenderDish dish={props.dish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.comments} />
+          <RenderComments comments={props.comments}
+        addComment={props.addComment}
+        dishId={props.dish.id}
+      />
           </div>
         </div>
       </div>
